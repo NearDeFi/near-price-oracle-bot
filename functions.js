bcb7d6b66fdd577199d8aadd828bbff892221a9f
Zavodil <support@zavodil.ru>
2021-09-10 22:42:34 +0300

    Switch to Decimals

--- functions.js
+++ functions.js
@@ -5,10 +5,18 @@ module.exports = {
         return new BN("1" + "0".repeat(power));
     },
 
+    /**
+     * @return {boolean}
+     */
     IsDifferentEnough: function(price_old, price_new) {
-        if (price_old.denominator.gt(new BN(0)) && price_old.denominator.cmp(price_new.denominator) !== 0)
+        //if (price_old.denominator.gt(new BN(0)) && price_old.denominator.cmp(price_new.denominator) !== 0)
+            //throw Error(`Illegal denominator`);
+
+        if (price_old.denominator > 0 && price_old.denominator != price_new.denominator)
             throw Error(`Illegal denominator`);
 
-        return (price_new.numerator.sub(price_old.numerator).abs().gte(price_old.numerator.div(new BN(1000)))); // 0.1%+ diff
+        //return (price_new.numerator.sub(price_old.numerator).abs().gte(price_old.numerator.div(new BN(1000)))); // 0.1%+ diff
+
+        return (Math.abs(price_new.multiplier - price_old.multiplier) >= price_old.multiplier * 0.001); // 0.5%+ diff
     }
 };
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const {CoinGeckoClient} = require('coingecko-api-v3');
-const BN = require('bn.js');
 const near = require('./near');
-const {GetBN, IsDifferentEnough} = require('./functions');
+const {IsDifferentEnough} = require('./functions');
 
-const contract = process.env.CONTRACT_ID || "dev-1631221522950-91597549533987";
+const contract = process.env.CONTRACT_ID || "dev-1631302633591-50236902542063";
 const account_id = process.env.NEAR_ACCOUNT_ID || 'zavodil.testnet';
 
 const client = new CoinGeckoClient({
@@ -14,27 +13,29 @@ const client = new CoinGeckoClient({
 console.log("Welcome to the Oracle Bot")
 
 let coins = {
-    "near": {coingecko: "near", denominator: GetBN(24)},
-    "eth": {coingecko: "ethereum", denominator: GetBN(18)},
-    "usdt": {coingecko: "tether", denominator: GetBN(6)},
-    "usdc": {coingecko: "usd-coin", denominator: GetBN(6)},
-    "dai": {coingecko: "dai", denominator: GetBN(18)}
+    "wrap.testnet": {coingecko: "near", decimals: 24},
+    "neth.ft-fin.testnet": {coingecko: "ethereum", decimals: 18},
+    "nusdt.ft-fin.testnet": {coingecko: "tether", decimals: 6},
+    "nusdc.ft-fin.testnet": {coingecko: "usd-coin", decimals: 6},
+    "ndai.ft-fin.testnet": {coingecko: "dai", decimals: 18}
 };
 
 const tickers = Object.keys(coins);
 
+// coingecko
+const fraction_digits = 2;
 client.simplePrice({ids: tickers.map(ticker => coins[ticker].coingecko).join(","), vs_currencies: "usd"})
     .then((rates) => {
         let new_prices = {};
 
         tickers.map(ticker => {
             const coin = coins[ticker];
-            const discrepancy_denominator = Math.min(coin.denominator, Math.pow(10, 12));
-            const rate = new BN(rates[coin.coingecko].usd * discrepancy_denominator);
+            const discrepancy_denominator = Math.pow(10, fraction_digits);
+            const rate = rates[coin.coingecko].usd * discrepancy_denominator;
 
             new_prices[ticker] = {
-                numerator: rate.mul(coin.denominator.div(new BN(discrepancy_denominator))),
-                denominator: coin.denominator
+                multiplier: rate,
+                decimals: Math.floor(coin.decimals + fraction_digits)
             };
         });
 
@@ -43,20 +44,20 @@ client.simplePrice({ids: tickers.map(ticker => coins[ticker].coingecko).join(","
             .then(old_prices => old_prices.reduce((obj, item) => Object.assign(obj, {
                 [item.asset_id]:
                     item.price
-                        ? {numerator: new BN(item.price.numerator), denominator: new BN(item.price.denominator)}
-                        : {numerator: new BN(0), denominator: new BN(0)}
+                        ? {multiplier: item.price.multiplier, decimals: item.price.decimals}
+                        : {multiplier: 0, decimals: 0}
             }), {}))
             .then(old_prices => {
                 let prices_to_update = [];
                 tickers.map(ticker => {
-                    console.log(`Compare ${ticker}: ${old_prices[ticker].numerator.toString()} and ${new_prices[ticker].numerator.toString()}`);
+                    console.log(`Compare ${ticker}: ${old_prices[ticker].multiplier.toString()} and ${new_prices[ticker].multiplier.toString()}`);
                     if (IsDifferentEnough(old_prices[ticker], new_prices[ticker])) {
                         console.log(`!!! Update ${ticker} price`)
                         prices_to_update.push({
                             asset_id: ticker,
                             price: {
-                                numerator: new_prices[ticker].numerator.toString(),
-                                denominator: new_prices[ticker].denominator.toString()
+                                multiplier: Math.floor(new_prices[ticker].multiplier).toString(),
+                                decimals: new_prices[ticker].decimals
                             }
                         })
                     }
@@ -67,17 +68,17 @@ client.simplePrice({ids: tickers.map(ticker => coins[ticker].coingecko).join(","
                         .then(resp => console.log(resp));
                 }
             })
-    })
+    });
 
 
 /*
 console.log(IsDifferentEnough(
     {
-        numerator: new BN(200),
+        multiplier: new BN(200),
         denominator: new BN(100),
     },
     {
-        numerator: new BN(0),
+        multiplier: new BN(0),
         denominator: new BN(100),
     }));
  */
