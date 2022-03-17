2d26bc1c58a844546174818c8d8e161cda92b85c
Evgeny Kuzyakov <ek@nearprotocol.com>
2022-03-17 13:02:28 -0700

    Fix binance futures for USDC

--- feeds/binance-futures.js
+++ feeds/binance-futures.js
@@ -1,17 +1,20 @@
-const Binance = require('binance-api-node').default
+const Binance = require("binance-api-node").default;
 
 module.exports = {
-    getPrices: async function (coins) {
-        const client = Binance();
+  getPrices: async function (coins) {
+    const client = Binance();
 
-        let tickers_to_process = Object.keys(coins)
-            .filter(ticker => coins[ticker].binance);
+    let tickers_to_process = Object.keys(coins).filter(
+      (ticker) => coins[ticker].binance
+    );
 
-        const prices = await client.futuresPrices();
+    const prices = await client.futuresPrices();
 
-        return tickers_to_process.reduce((object, ticker) => {
-            object[ticker] = parseFloat(prices[coins[ticker].binance]);
-            return object
-        }, {});
-    }
-}
+    return tickers_to_process.reduce((object, ticker) => {
+      if (ticker in prices) {
+        object[ticker] = parseFloat(prices[ticker]);
+      }
+      return object;
+    }, {});
+  },
+};
