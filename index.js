266b6d8d04fae83d10e7f9ba07f54e6e950d8396
Vadim <support@zavodil.ru>
2023-11-02 18:16:39 +0200

    fix: eth rpc updated fix: usd ticker from gate removed

--- index.js
+++ index.js
@@ -125,7 +125,6 @@ const MainnetCoins = {
     decimals: 6,
     stablecoin: true,
     coingecko: "tether",
-    gate: "usdt_usd",
     chainlink: "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D"
   },
   "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near": {
